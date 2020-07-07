d087ebb38903c45677faeb12f99b69ae43098004
Zac Johnston <zacjohn@msu.edu>
2020-07-07 13:25:53 -0400

    [submit_job.sh] updated

--- submit_job.sh
+++ submit_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash --login
 ########## SBATCH Lines for Resource Request ##########
  
-#SBATCH --time=00:30:00       # limit of wall clock time - how long the job will run (same as -t)
+#SBATCH --time=15:00:00       # limit of wall clock time - how long the job will run (same as -t)
 #SBATCH --nodes=1             # number of different nodes - could be an exact number or a range of nodes (same as -N)
 #SBATCH --ntasks=1            # number of tasks - how many tasks (nodes) that you require (same as -n)
 #SBATCH --cpus-per-task=1     # number of CPUs (or cores) per task (same as -c)
@@ -16,9 +16,10 @@ module load HDF5/1.10
 module load GSL
 
 conda activate flashbang
-cd ${HOME}/flash2snowglobes
+cd /mnt/gs18/scratch/users/john7457/flash2snowglobes
+#cd ${HOME}/flash2snowglobes
 
-python testrun.py
+python flash2snowglobes.py
  
 scontrol show job $SLURM_JOB_ID     ### write job information to output file
 seff $SLURM_JOBID                   ### Resource usage info
