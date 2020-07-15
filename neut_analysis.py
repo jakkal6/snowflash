4e3eec84bcbc0f3dc3ddff95e0e48937e9f49546
Zac Johnston <zacpjohnston@gmail.com>
2020-07-15 13:11:12 -0400

    [neut_analysis.py] merged test_neut.py

--- neut_analysis.py
+++ neut_analysis.py
@@ -1,95 +1,195 @@
 import numpy as np
+import os
+import pandas as pd
 
-def analysis(a,m,timebins,output,detector,nomix_tot):
-        """Does analysis on snowglobes output and writes out to ascii files in output \n
+
+def analysis(a, m, output, detector, nomix_tot):
+    """Does analysis on snowglobes output and writes out to ascii files in output \n
         Kinda a mess since it's hacked in from historical scripts \n
         Currently calculating mean energy and total counts for each detector channel \n
         for each timestep and calculating time-integrated mean energies \n
-        Input: alpha, mass, timebins for snowglobes code [s], \n
+        Input: alpha, mass, for snowglobes code [s], \n
         output directory path, detector configuration\n
         nomix_tot file for time-integrated quantities \n
         Output: None"""
 
-        nomix_time = open(output+"/nomix_analysis_a"+str(a)+"_m"+str(m)+".dat","w")
-        nomix_time.write("Time \t Avg_Total \t Avg_IBD \t Avg_ES \t Avg_nu_e-O16 \t Avg_anu_e-O16 \t Avg_NC \
-                \t Tot_Total \t Tot_IBD \t Tot_ES \t Tot_nu_e-O16 \t Tot_anu_e-O16 \t Tot_NC \n")
+    groups = {'IBD': ['ibd'],
+              'ES': ['nue_e'],
+              'nu_e_O16': ['nue_O16'],
+              'anu_e_O16': ['nuebar_O16'],
+              'NC': ['nc_nue_O16', 'nc_nuebar_O16',
+                     'nc_numu_O16', 'nc_numubar_O16',
+                     'nc_nutau_O16', 'nc_nutaubar_O16']
+              }
+    channels = get_all_channels(groups)
 
-        time = np.loadtxt('./fluxes/pinched_a'+str(a)+'_m'+str(m)+'_key.dat',skiprows=1,usecols=(1),unpack=True)
+    time_filepath = os.path.join('./fluxes/', f'pinched_a{a}_m{m}_key.dat')
+    time = np.loadtxt(time_filepath, skiprows=1, usecols=[1], unpack=True)
+    n_time = len(time)
 
-        nomix_ibd = np.zeros(200)  # hold time-integrated counts for each energy bin
-        nomix_es = np.zeros(200)
-        nomix_eo16 = np.zeros(200)
-        nomix_ao16 = np.zeros(200)
-        nomix_nc = np.zeros(200)
-        nomix_total = np.zeros(200)
-        for i in range(1,len(timebins)+1):
-            en1,ibd1 = np.genfromtxt('./out/pinched_a'+str(a)+'_m'+str(m)+'_'+str(i)+'_ibd_'+str(detector)+'_events_smeared.dat', skip_footer = 2,usecols=(0,1), unpack=True)
-            es1 = np.genfromtxt('./out/pinched_a'+str(a)+'_m'+str(m)+'_'+str(i)+'_nue_e_'+str(detector)+'_events_smeared.dat', skip_footer = 2,usecols=(1), unpack=True)
-            eo161 = np.genfromtxt('./out/pinched_a'+str(a)+'_m'+str(m)+'_'+str(i)+'_nue_O16_'+str(detector)+'_events_smeared.dat',skip_footer = 2, usecols=(1), unpack=True)
-            ao161 = np.genfromtxt('./out/pinched_a'+str(a)+'_m'+str(m)+'_'+str(i)+'_nuebar_O16_'+str(detector)+'_events_smeared.dat',skip_footer = 2, usecols=(1), unpack=True)
-            enc1 = np.genfromtxt('./out/pinched_a'+str(a)+'_m'+str(m)+'_'+str(i)+'_nc_nue_O16_'+str(detector)+'_events_smeared.dat',skip_footer = 2, usecols=(1), unpack=True)
-            anc1 = np.genfromtxt('./out/pinched_a'+str(a)+'_m'+str(m)+'_'+str(i)+'_nc_nuebar_O16_'+str(detector)+'_events_smeared.dat',skip_footer = 2, usecols=(1), unpack=True)
-            munc1 = np.genfromtxt('./out/pinched_a'+str(a)+'_m'+str(m)+'_'+str(i)+'_nc_numu_O16_'+str(detector)+'_events_smeared.dat',skip_footer = 2, usecols=(1), unpack=True)
-            mubarnc1 = np.genfromtxt('./out/pinched_a'+str(a)+'_m'+str(m)+'_'+str(i)+'_nc_numubar_O16_'+str(detector)+'_events_smeared.dat',skip_footer = 2, usecols=(1), unpack=True)
-            taunc1 = np.genfromtxt('./out/pinched_a'+str(a)+'_m'+str(m)+'_'+str(i)+'_nc_nutau_O16_'+str(detector)+'_events_smeared.dat',skip_footer = 2, usecols=(1), unpack=True)
-            taubarnc1 = np.genfromtxt('./out/pinched_a'+str(a)+'_m'+str(m)+'_'+str(i)+'_nc_nutaubar_O16_'+str(detector)+'_events_smeared.dat',skip_footer = 2, usecols=(1), unpack=True)
+    energy_bins = load_energy_bins(channel=channels[0], i=1,
+                                   a=a, m=m, detector=detector)
+    n_bins = len(energy_bins)
 
-            nomix_en = en1  # energy bins?
-            tot1 = ibd1 + es1 + eo161+ao161+enc1+anc1+munc1+mubarnc1+taunc1+taubarnc1
-            nomix_total += tot1  # add to time-integration
-            nomix_ibd += ibd1
-            nomix_es += es1
-            nomix_eo16 += eo161
-            nomix_ao16 += ao161
-            nc1 = enc1 + anc1 + munc1 + mubarnc1 + taunc1 + taubarnc1
-            nomix_nc += nc1
+    integrated_counts = {'All': np.zeros(n_bins)}
+    time_totals = {'All': np.zeros(n_time)}
+    time_avg = {'All': np.zeros(n_time)}
 
-            tot_tot = sum(tot1)
-            if(sum(tot1) != 0.0 ):
-              av_tot = sum(tot1*en1*1000)/sum(tot1)
-            else:
-              av_tot = 0.0
+    for group in groups:
+        integrated_counts[group] = np.zeros(n_bins)
+        time_totals[group] = np.zeros(n_time)
+        time_avg[group] = np.zeros(n_time)
 
-            tot_ibd = sum(ibd1)
-            if(sum(ibd1) != 0.0 ) :
-              av_ibd = sum(ibd1*en1*1000)/sum(ibd1)
-            else:
-              av_ibd = 0.0
+    for i in range(1, n_time + 1):
+        channel_counts = load_channel_counts(channels=channels,
+                                             i=i, a=a, m=m,
+                                             detector=detector)
 
-            tot_es = sum(es1)
-            if(sum(es1) != 0.0):
-              av_es = sum(es1*en1*1000)/sum(es1)
-            else:
-              av_es = 0.0
+        group_counts = get_group_counts(channel_counts,
+                                        groups=groups,
+                                        n_bins=n_bins)
 
-            tot_eo16 = sum(eo161)
-            if(sum(eo161) != 0.0):
-              av_eo16 = sum(eo161*en1*1000)/sum(eo161)
-            else:
-              av_eo16 = 0.0
+        for group in integrated_counts:
+            integrated_counts[group] += group_counts[group]
 
-            tot_ao16 = sum(ao161)
-            if(sum(ao161) != 0.0):
-              av_ao16 = sum(ao161*en1*1000)/sum(ao161)
-            else:
-              av_ao16 = 0.0
+        group_totals = get_totals(group_counts)
 
-            tot_nc = sum(nc1)
-            if(sum(nc1) != 0.0):
-              av_nc = sum(nc1*en1*1000)/sum(nc1)
-            else:
-              av_nc = 0.0
+        group_avg = get_avg(group_counts=group_counts,
+                            group_totals=group_totals,
+                            energy_bins=energy_bins)
 
-            nomix_time.write(str(time[i-1])+"\t"+str(av_tot)+"\t"+str(av_ibd)+ \
-                    "\t"+str(av_es)+"\t"+str(av_eo16)+"\t"+str(av_ao16)+"\t"+str(av_nc)+ \
-                    "\t"+str(tot_tot)+"\t"+str(tot_ibd)+"\t"+str(tot_es)+"\t"+str(tot_eo16)+ \
-                    "\t"+str(tot_ao16)+"\t"+str(tot_nc)+"\n")
+        for group in group_totals:
+            time_totals[group][i-1] = group_totals[group]
+            time_avg[group][i-1] = group_avg[group]
 
-        nomix_time.close()
-        nomix_avg_tot = sum(nomix_total*nomix_en*1000)/sum(nomix_total)
-        nomix_avg_ibd = sum(nomix_ibd*nomix_en*1000)/sum(nomix_ibd)
-        nomix_avg_es = sum(nomix_es*nomix_en*1000)/sum(nomix_es)
-        nomix_avg_eo16 = sum(nomix_eo16*nomix_en*1000)/sum(nomix_eo16)
-        nomix_avg_ao16 = sum(nomix_ao16*nomix_en*1000)/sum(nomix_ao16)
-        nomix_avg_nc = sum(nomix_nc*nomix_en*1000)/sum(nomix_nc)
-        nomix_tot.write(str(m)+"\t"+str(nomix_avg_tot)+"\t"+str(nomix_avg_ibd)+"\t"+str(nomix_avg_es)+"\t"+str(nomix_avg_eo16)+"\t"+str(nomix_avg_ao16)+"\t"+str(nomix_avg_nc)+"\n")
+    time_table = create_time_table(timesteps=time,
+                                   time_totals=time_totals,
+                                   time_avg=time_avg)
+
+    save_time_table(table=time_table, detector=detector,
+                    a=a, m=m, output=output)
+
+    # TODO: write integrated row
+
+    integrated_totals = get_totals(integrated_counts)
+    integrated_avg = get_avg(group_counts=integrated_counts,
+                             group_totals=integrated_totals,
+                             energy_bins=energy_bins)
+
+
+# ===========================================================
+#                   Raw channel counts
+# ===========================================================
+def get_all_channels(groups):
+    """Extract list of channels from dict of channel groups
+    """
+    channels = []
+    for group, subs in groups.items():
+        channels += subs
+
+    return channels
+
+
+def load_channel_counts(channels, i, a, m, detector):
+    """Load all raw channel counts into dict
+    """
+    channel_counts = {}  # arrays of channel counts per energy bin
+    for chan in channels:
+        channel_counts[chan] = load_channel_dat(channel=chan, i=i, a=a,
+                                                m=m, detector=detector)
+    return channel_counts
+
+
+def load_channel_dat(channel, i, a, m, detector):
+    """Load array of detection counts per energy bin
+    """
+    filepath = channel_dat_filepath(channel=channel, i=i, a=a, m=m, detector=detector)
+    return np.genfromtxt(filepath, skip_footer=2, usecols=[1], unpack=True)
+
+
+def channel_dat_filepath(channel, i, a, m, detector):
+    """Return filepath to snowglobes output file
+    """
+    return f'./out/pinched_a{a}_m{m}_{i}_{channel}_{detector}_events_smeared.dat'
+
+
+def load_energy_bins(channel, i, a, m, detector):
+    """Load array of energy bins (MeV) from a snowglobes output file
+    """
+    filepath = channel_dat_filepath(channel=channel, i=i, a=a, m=m, detector=detector)
+    energy_bins = np.genfromtxt(filepath, skip_footer=2, usecols=[0], unpack=True)
+    return energy_bins * 1000
+
+
+# ===========================================================
+#                   Group counts/averages
+# ===========================================================
+def get_group_counts(channel_counts, groups, n_bins):
+    """Sum channel counts by group
+    """
+    group_counts = {'All': np.zeros(n_bins)}
+
+    for group, sub_channels in groups.items():
+        counts = np.zeros(n_bins)
+
+        for chan in sub_channels:
+            counts += channel_counts[chan]
+
+        group_counts['All'] += counts
+        group_counts[group] = counts
+
+    return group_counts
+
+
+def get_totals(group_counts):
+    """Get total counts over all energy bins
+    """
+    totals = {}
+    for group in group_counts:
+        totals[group] = np.sum(group_counts[group])
+
+    return totals
+
+
+def get_avg(group_counts, group_totals, energy_bins):
+    """Get group average energies
+    """
+    group_avg = {}
+
+    for group, total in group_totals.items():
+        if total != 0:
+            avg = np.sum(group_counts[group] * energy_bins) / total
+        else:
+            avg = 0
+
+        group_avg[group] = avg
+
+    return group_avg
+
+
+# ===========================================================
+#                   Time-dependent data
+# ===========================================================
+def create_time_table(timesteps, time_totals, time_avg):
+    """Construct a DataFrame from time-dependent arrays of mean energies/total counts
+    """
+    table = pd.DataFrame()
+    table['Time'] = timesteps
+
+    for group in time_avg:
+        table[f'Avg_{group}'] = time_avg[group]
+
+    for group in time_totals:
+        table[f'Tot_{group}'] = time_totals[group]
+
+    return table
+
+
+def save_time_table(table, detector, a, m, output):
+    """Save time-dependent table to file
+    """
+    filepath = os.path.join(output, f"{detector}_analysis_a{a}_m{m}.dat")
+    string = table.to_string(index=False, justify='left')
+
+    with open(filepath, 'w') as f:
+        f.write(string)
