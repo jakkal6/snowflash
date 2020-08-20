e404b367a687a4e4f0a24233e9171a4e7d003168
Zac Johnston <zacpjohnston@gmail.com>
2020-08-20 16:33:43 -0400

    [snowglobes.py] added load_mass_tables()

--- snowglobes.py
+++ snowglobes.py
@@ -6,7 +6,8 @@ class SnowGlobesData:
     def __init__(self,
                  model_sets=('LMP', 'LMP+N50', 'SNA'),
                  alphas=(1, 2, 3),
-                 detector='ar40kt'):
+                 detector='ar40kt',
+                 load_data=True):
         """Collection of SnowGlobes data
 
         parameters
@@ -21,15 +22,23 @@ class SnowGlobesData:
         self.detector = detector
         self.model_sets = model_sets
         self.alphas = alphas
-        self.summary_tables = None
         self.mass_list = config.mass_list
+        self.n_mass = len(self.mass_list)
+
+        self.summary_tables = None
+        self.mass_tables = None
+
+        if load_data:
+            self.load_summary_tables()
+            self.load_mass_tables()
 
     # ===============================================================
     #                      Load Tables
     # ===============================================================
     def load_summary_tables(self):
-        """Load time-integrated summary tables
+        """Load all time-integrated summary tables
         """
+        print('Loading summary tables')
         tables = dict.fromkeys(self.model_sets)
 
         for i, alpha in enumerate(self.alphas):
@@ -37,4 +46,24 @@ class SnowGlobesData:
             tables[model_set] = snow_tools.load_summary_table(alpha=alpha,
                                                               detector=self.detector)
         self.summary_tables = tables
+
+    def load_mass_tables(self):
+        """Load time-dependent tables for all individual mass models
+        """
+        tables = dict.fromkeys(self.model_sets, {})
+
+        for i, alpha in enumerate(self.alphas):
+            model_set = self.model_sets[i]
+
+            for j, mass in enumerate(self.mass_list):
+                print(f'\rLoading mass tables, {model_set}: {j+1}/{self.n_mass}', end='')
+
+                table = snow_tools.load_mass_table(mass=mass,
+                                                   alpha=alpha,
+                                                   detector=self.detector)
+                tables[model_set][mass] = table
+
+            print()
+
+        self.mass_tables = tables
 
