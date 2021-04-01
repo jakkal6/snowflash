7f918e0ecca5f1bc96a99e22982c06c11a63b0e6
Zac Johnston <zacpjohnston@gmail.com>
2021-04-01 17:56:41 -0400

    [config.py] tweak labels

--- snowglobes/config.py
+++ snowglobes/config.py
@@ -44,7 +44,7 @@ ax_scales = {
 ax_labels = {
     'mass': r'ZAMS Mass [$\mathrm{M}_\odot$]',
     'Time': 'Time post-bounce [s]',
-    'Avg': r'${\langle E \rangle}_\mathrm{tot}$ [MeV]',
+    'Avg': r'${\langle E \rangle}_\mathrm{tot}$ [$\mathrm{MeV}$]',
     'Tot': r'$\mathrm{N_{tot}}$',
     'm_fe': r'$M_\mathrm{Fe}$ [$\mathrm{M}_\odot$]',
 }
