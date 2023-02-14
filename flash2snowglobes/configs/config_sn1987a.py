691d44d576fa4129f784106d083a40b96df57b8b
Zac Johnston <zacpjohnston@gmail.com>
2023-02-14 15:31:46 -0500

    [configs/] created prelim config for sn1987a

---
+++ flash2snowglobes/configs/config_sn1987a.py
@@ -0,0 +1,27 @@
+# ===== Model setup =====
+detector = "ar40kt"
+mixing = ['nomix', 'normal', 'inverted']
+model_sets = ['sn1987a']
+
+# names used in sim files (if different from model_set)
+model_set_map = {}
+
+# paths
+snowglobes_path = "/mnt/research/SNAPhU/zac/snowglobes"
+models_path = '/mnt/research/SNAPhU/sn1987a/runs'
+
+# progenitor ZAMS masses [Msun]
+masses = ('15-7b', '15-8b', '16-4a', '16-7b', '17-7a')
+
+# source distance [kpc]
+distance = 10
+
+# time bins [s]
+t_start = -0.05  # relative to bounce
+t_end = 1.0
+dt = 0.005
+
+# energy bins [GeV]
+e_start = 0.0
+e_end = 0.1
+e_step = 0.0002
