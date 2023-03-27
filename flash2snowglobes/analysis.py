3b0d0baa9913e311fa5d363ff0e2c5d606ccc01f
Zac Johnston <zacpjohnston@gmail.com>
2023-03-27 16:20:34 -0400

    [snow_data.py] refactor data paths

--- flash2snowglobes/analysis.py
+++ flash2snowglobes/analysis.py
@@ -277,7 +277,7 @@ def save_timebin_table(table, detector, model_set, mass, mixing):
     mass : str, int or float
     mixing : str
     """
-    path = os.path.join('timebin', model_set, detector, mixing)
+    path = os.path.join('output', model_set, detector, mixing)
 
     if not os.path.isdir(path):
         os.makedirs(path)
