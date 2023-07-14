619203265be002722d6385975269fade9d1a7a38
Zac Johnston <zacpjohnston@gmail.com>
2023-07-14 17:57:19 -0400

    [snow_model.py] added plot_sum()

--- snowflash/snow/snow_model.py
+++ snowflash/snow/snow_model.py
@@ -47,10 +47,7 @@ class SnowModel:
     def plot_energy(self,
                     t_bin,
                     channels=None,
-                    x_lims=None,
-                    y_lims=None,
                     ax=None,
-                    legend=True,
                     title=True,
                     data_only=False,
                     ):
@@ -61,10 +58,7 @@ class SnowModel:
         t_bin : flt
             time bin to plot
         channels : [str]
-        x_lims : [low, high]
-        y_lims : [low, high]
         ax : Axis
-        legend : bool
         title : bool
         data_only : bool
         """
@@ -73,9 +67,30 @@ class SnowModel:
                             fixed_bin='time',
                             fixed_value=t_bin,
                             channels=channels,
-                            x_lims=x_lims,
-                            y_lims=y_lims,
                             ax=ax,
-                            legend=legend,
                             title=title,
+                            data_only=data_only)
+
+    def plot_sum(self,
+                 x_bin,
+                 sum_bin,
+                 channels=None,
+                 ax=None,
+                 data_only=False,
+                 ):
+        """Plot energy bins for given time
+
+        parameters
+        ----------
+        x_bin : str
+        sum_bin : str
+        channels : [str]
+        ax : Axis
+        data_only : bool
+        """
+        snow_plot.plot_bins(counts=self.counts.sum(sum_bin),
+                            x_bin=x_bin,
+                            channels=channels,
+                            ax=ax,
+                            title=False,
                             data_only=data_only)
